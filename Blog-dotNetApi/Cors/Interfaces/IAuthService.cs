a4489bba9a717a603e82dd78fa31f8cb16b24b55
Erfan <erfuun.mi82@gmail.com>
2024-07-27 02:23:23 -0700

    Api is ready for testing in step 1

---
+++ Blog-dotNetApi/Cors/Interfaces/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using Blog_dotNetApi.Cors.Dtos;
+
+namespace Blog_dotNetApi.Cors.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthServiceResponseDto> SeedRolesAsync();
+        Task<AuthServiceResponseDto> RegisterAsync(RegisterDto registerDto);
+        Task<AuthServiceResponseDto> LoginAsync(LoginDto loginDto);
+        Task<AuthServiceResponseDto> MakeAdminAsync(UpdatePermissionDto updatePermissionDto);
+        Task<AuthServiceResponseDto> MakeOwnerAsync(UpdatePermissionDto updatePermissionDto);
+    }
+}
